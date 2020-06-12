3d9ffb8197d98b0a9700d3c7df10e3cdf69aa7c2
younjong park <younjongpark@MacBook-Pro.local>
2020-06-12 14:42:59 +0900

    create reactproject

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React , {Component} from 'react';
-import MyComponent from './MyComponent'; // MyComponent 파일을 불러옵니다.
-import './App.css';
+import EventPractice from './EventPractice';
 
 class App extends Component{
   render(){
     return (
-      <MyComponent name="React" age={4}/>
+      <EventPractice/>
     );
   }
 }
